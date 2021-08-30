5ffb1ed06135d286c818b30ffa00c3c12ad9464e
Demid <bydimko@yandex.ru>
2021-08-30 15:31:26 +0300

    lesson2 updated

--- products/views.py
+++ products/views.py
@@ -25,31 +25,31 @@ def products(request):
                 'name': 'Синяя куртка The North Face',
                 'price': '23 725,00',
                 'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
-                'img': 'static/vendor/img/products/Blue-jacket-The-North-Face.png'
+                'img': 'vendor/img/products/Blue-jacket-The-North-Face.png'
             },
             {
                 'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
                 'price': '3 390,00',
                 'description': 'Материал с плюшевой текстурой. Удобный и мягкий.',
-                'img': 'static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png'
+                'img': 'vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png'
             },
             {
                 'name': 'Черный рюкзак Nike Heritage',
                 'price': '2 340,00',
                 'description': 'Плотная ткань. Легкий материал.',
-                'img': 'static/vendor/img/products/Black-Nike-Heritage-backpack.png'
+                'img': 'vendor/img/products/Black-Nike-Heritage-backpack.png'
             },
             {
                 'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex',
                 'price': '13 590,00',
                 'description': 'Гладкий кожаный верх. Натуральный материал.',
-                'img': 'static/vendor/img/products/Black-Dr-Martens-shoes.png'
+                'img': 'vendor/img/products/Black-Dr-Martens-shoes.png'
             },
             {
                 'name': 'Темно-синие широкие строгие брюки ASOS DESIGN',
                 'price': '2 890,00',
                 'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.',
-                'img': 'static/vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png'},
+                'img': 'vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png'},
         ]
     }
 
